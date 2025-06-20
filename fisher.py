95c451d40291a93cd9f979aad4b4dbfe2e4d0049
илья <ilya18kum@gmail.com>
2025-06-20 14:23:26 +0300

    minor upd

--- fisher.py
+++ fisher.py
@@ -2,10 +2,8 @@ from aiogram import Dispatcher, Bot
 import asyncio
 from config import TOKEN
 from handler.commands import command_router
-from handler.commands import callbacks_router
 dp = Dispatcher()
 dp.include_router(command_router)
-dp.include_router(callbacks_router)
 async def main():
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
--- handler/callback.py
+++
@@ -1,9 +0,0 @@
-from gc import callbacks
-from aiogram import Dispatcher
-from aiogram import types
-import aiogram
-from aiogram.filters import Command
-from aiogram import F, Router
-
-callbacks_router = Router()
-
